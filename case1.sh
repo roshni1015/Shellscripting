ecc5e5f62ca684b43227d47f1ecd52e32d5f0acc
roshni1015 <99880059+roshni1015@users.noreply.github.com>
2022-02-17 23:22:39 +0530

    I am pushing some assignment

---
+++ case1.sh
@@ -0,0 +1,38 @@
+read -p "Enter Some Name Here " name
+
+
+
+
+case $name in
+
+   Virat)
+
+      echo "$name Is a captain of RCB"
+
+      ;;
+
+   Rohit)
+
+      echo "$name Is a captain of MI"
+
+      ;;
+
+   Dhoni)
+
+      echo "$name Is a captain of CSK"
+
+      ;;
+
+   Rahul)
+
+      echo "$name Is a captain of PBKS"
+
+      ;;
+
+   *)
+
+     echo "Sorry! Please check your input , Its not accepting our creteria :("
+
+     ;;
+
+esac
